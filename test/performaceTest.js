2225bb4df5d526c9288a08188cfa3c73baf1e6f8
wapj <wapj2000@gmail.com>
2013-09-09 17:31:55 +0900

    performance tuning for HashRing.js

--- test/performaceTest.js
+++ test/performaceTest.js
@@ -1,8 +1,34 @@
-/**
- * Created with JetBrains WebStorm.
- * User: andy
- * Date: 13. 9. 9.
- * Time: 오후 4:14
- * For F4Alien
- * Podotree. All rights reserved.
- */
+var HashRing = require('../lib/HashRing');
+var kakaoDummyId = "12345678901234567";
+
+var nodes = [];
+for(var i = 0;i < 100; i++){
+  nodes.push("node" + i);
+}
+
+//Check HashRing GetNode
+var ring = new HashRing(nodes);
+var start = new Date();
+for(var i =0;i<1000000;i++){
+  ring.getNode(kakaoDummyId + i);
+}
+var end = new Date();
+console.log("[getNode] elapsed time : %s ms", (end - start));
+
+
+//Check HashValue with crc32 algorithm
+var hashValue = require("../lib/HashValue");
+start = new Date();
+for (var i =0; i < 1000000; i++){
+  hashValue.hash(kakaoDummyId + i, "crc32");
+}
+console.log("[crc32] elapsed time : %s ms", (new Date() - start));
+
+
+//Check HashValue with md5 algorithm
+var hashValue = require("../lib/HashValue");
+start = new Date();
+for (var i =0; i < 1000000; i++){
+  hashValue.hash(kakaoDummyId + i, "md5");
+}
+console.log("[md5] elapsed time : %s ms", (new Date() - start));
